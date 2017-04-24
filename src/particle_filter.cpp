a7de6aaa5e70b3f82f907b4442e161b2e62bf8b3
Celien Nanson <cesliens@gmail.com>
2017-04-24 14:18:19 +0200

    First try for updateWeights function. Still too large x error

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -127,13 +127,82 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[], s
 	//   http://planning.cs.uiuc.edu/node99.html
 
 	// Extract value
-	double sigma_x = std_landmark[0];
-	double sigma_y = std_landmark[1]; 
+	double std_x = std_landmark[0];
+	double std_y = std_landmark[1];
+	double weights_sum = 0; // For normalization
 
-	// For each particle, convert observations from Vehicle to maps system
+	// For each particle: 
 	for(int i = 0; i < num_particles ; i++){
 		Particle *p = &particles[i];
+		double weight = 1.0;
+
+		for(int j = 0; j < observations.size(); j++){
+			// Convert observations from Vehicle to Map space
+			LandmarkObs current_obs = observations[j];
+			LandmarkObs current_obs_transformed; 
+			double vx = current_obs.x * cos(p->theta) - current_obs.y * sin(p->theta) + p->x;
+			double vy = current_obs.x * sin(p->theta) + current_obs.y * cos(p->theta) + p->y;
+			current_obs_transformed.x = vx;
+			current_obs_transformed.y = vy;
+			current_obs_transformed.id = current_obs.id;  
+
+			// Associate the closest measurement to a given landmark
+			Map::single_landmark_s landmark; 
+			double smallest_dist = std::numeric_limits<double>::max(); // https://stackoverflow.com/questions/409348/iteration-over-stdvector-unsigned-vs-signed-index-variable
+			for(int k = 0; k < map_landmarks.landmark_list.size(); k++){ // For each landmark
+				Map::single_landmark_s current_lm = map_landmarks.landmark_list[k];
+				double dist_pred_obs = dist(current_obs_transformed.x, current_obs_transformed.x, current_lm.x_f, current_lm.y_f);
+				if(dist_pred_obs < smallest_dist){
+					smallest_dist = dist_pred_obs; 
+					landmark = current_lm; 
+				}
+			}
+
+			// Update weights
+			double numerator = exp(-0.5 * (pow((current_obs_transformed.x - landmark.x_f), 2) / pow(std_x, 2) + pow((current_obs_transformed.y - landmark.y_f), 2) / pow(std_y, 2)));
+			double denominator = 2 * M_PI * std_x * std_y;
+			weight *= numerator/denominator; 
+		}
+
+		p->weight = weight; 
+		weights_sum += weight;
 		
+		/*// Convert observations from Vehicle to Map space
+		vector<LandmarkObs> observations_map(observations);
+		for(int j = 0; j < observations_map.size(); j++){
+			LandmarkObs *current_obs = &observations_map[j]; 
+			double vx = current_obs->x * cos(p->theta) - current_obs->y * sin(p->theta) + p->x;
+			double vy = current_obs->x * sin(p->theta) + current_obs->y * cos(p->theta) + p->y;
+			current_obs->x = vx;
+			current_obs->y = vy;
+			current_obs->id = 0; 
+		}
+
+		// Predict landmark regarding the sensor's range
+		vector<LandmarkObs> predicted;
+		for (auto landmark : map_landmarks.landmark_list) {
+			if (dist(p->x, p->y, landmark.x_f, landmark.y_f) < sensor_range) {
+        		predicted.push_back({landmark.id_i, landmark.x_f, landmark.y_f});
+      		}
+    	}
+
+    	// Find nearest neighbor
+    	dataAssociation(predicted, observations); 
+
+    	// Update particle weight with multi-variate Gaussian ditribution
+    	double weight = 1.0;
+    	double numerator = exp(-0.5 * (pow((current_obs.x - best_lm.x_f), 2) / pow(std_x, 2) + pow((current_obs.y - best_lm.y_f), 2) / pow(std_y, 2)));
+    	double denominator = 2 * M_PI * std_x * std_y;
+    	weight *= numerator/denominator; 
+    	p->weight = weight; 
+    	weights_sum += weight; */
+	}
+
+	// Normalize
+	for (int i = 0; i < num_particles; i++) {
+		Particle *p = &particles[i];
+		p->weight /= weights_sum;
+		weights[i] = p->weight;
 	}
 }
 
